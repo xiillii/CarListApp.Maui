e558ff6ad977abb7527a254c1703ebbd4d5835a1
José Alonso <mail@josealonso.dev>
2022-08-11 23:57:09 -0500

    Creating the data model

---
+++ CarListApp.Maui/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarListApp.Maui.Models;
+
+public  class Car
+{
+    public int Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string Vin { get; set; }
+}
