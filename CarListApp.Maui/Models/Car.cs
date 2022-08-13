b2e6f0089693fca8789c1f5d8c82992c9d360c4e
José Alonso <mail@josealonso.dev>
2022-08-13 00:55:32 -0500

    Creating a BaseModel class, and a BaseViewModel class with CommunityToolkit MVVM Observable Object

--- CarListApp.Maui/Models/Car.cs
+++ CarListApp.Maui/Models/Car.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace CarListApp.Maui.Models;
 
-namespace CarListApp.Maui.Models;
-
-public  class Car
+public  class Car : BaseEntity
 {
-    public int Id { get; set; }
     public string Make { get; set; }
     public string Model { get; set; }
     public string Vin { get; set; }
---
+++ CarListApp.Maui/ViewModels/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CarListApp.Maui.ViewModels;
+
+public partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _title;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotLoading))]
+    private bool _isLoading;
+
+    public bool IsNotLoading => !_isLoading;
+}
